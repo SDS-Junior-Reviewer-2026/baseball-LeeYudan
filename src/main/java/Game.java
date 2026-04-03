a28520ea7cfb585ef8a641e85488defcbea30a93
sakamii <dalnara608@gmail.com>
2026-04-03 13:37:04 +0900

    중복 숫자 없게 하는 코드 추가

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -12,5 +12,12 @@ public class Game {
                 throw new IllegalArgumentException("You must enter a number");
             }
         }
+
+        if(guessNumber.charAt(0)== guessNumber.charAt(1)||
+        guessNumber.charAt(0)==guessNumber.charAt(2)||
+        guessNumber.charAt(1)==guessNumber.charAt(2)){
+            throw new IllegalArgumentException("You must not enter same number");
+        }
+
     }
 }
