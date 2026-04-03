05cc7eeb4e155ced4fdb56cabaa6b6f6f05940b3
sakamii <dalnara608@gmail.com>
2026-04-03 13:59:30 +0900

    정답 맞추는 기능, Result 클래스 추가

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,23 +1,38 @@
 public class Game {
-    public void guess(String guessNumber) {
-        if(guessNumber==null){
+
+    public String question;
+
+    public GuessResult guess(String guessNumber) {
+        assertIllegalArgument(guessNumber);
+        if(guessNumber.equals(question)){
+            return new GuessResult(true,3,0);
+        }else{
+            return null;
+        }
+    }
+
+    private void assertIllegalArgument(String guessNumber) {
+        if(guessNumber ==null){
             throw new IllegalArgumentException("You must enter");
         }
 
         if(guessNumber.length() !=3){
             throw new IllegalArgumentException("You must enter THREE number");
         }
-        for(char number:guessNumber.toCharArray()){
+        for(char number: guessNumber.toCharArray()){
             if(number<'0'||number>'9'){
                 throw new IllegalArgumentException("You must enter a number");
             }
         }
 
-        if(guessNumber.charAt(0)== guessNumber.charAt(1)||
-        guessNumber.charAt(0)==guessNumber.charAt(2)||
-        guessNumber.charAt(1)==guessNumber.charAt(2)){
+        if(isDuplicatedNumber(guessNumber)){
             throw new IllegalArgumentException("You must not enter same number");
         }
+    }
 
+    private boolean isDuplicatedNumber(String guessNumber) {
+        return guessNumber.charAt(0) == guessNumber.charAt(1) ||
+                guessNumber.charAt(0) == guessNumber.charAt(2) ||
+                guessNumber.charAt(1) == guessNumber.charAt(2);
     }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameTest {
@@ -25,10 +26,21 @@ public class GameTest {
         assertIllegalArgument("12s");
     }
 
+    @Test
+    void returnSolvedResultIfMatchedNumber(){
+        game.question = "123";
+        GuessResult result = game.guess("123");
+        assertThat(result).isNotNull();
+        assertThat(result.isSolved()).isEqualTo(true);
+        assertThat(result.getStrikes()).isEqualTo(3);
+        assertThat(result.getBalls()).isEqualTo(0);
+    }
+
+
     private void assertIllegalArgument(String guessNumber) {
         try {
             game.guess(guessNumber);
-            fail();
+            org.junit.jupiter.api.Assertions.fail();
         } catch (IllegalArgumentException e) {
 
         }
